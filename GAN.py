c51a8c7e306d443ef2ac8c41ea290ddcc6d130b7
Will Macdonald <16wjgm@caslab.queensu.ca>
2019-10-28 18:51:03 -0400

    Adding TrainGAN file to repo

--- GAN.py
+++ GAN.py
@@ -1,8 +1,12 @@
+# import necessary files
+
+
 # import functions from supporting files
 from Generator import init_generator
 from Discriminator import init_discriminator
 from Detector import init_detector
 from LoadData import load_dataset
+from TrainGAN import train
 
 
 # compile the functionality of GAN
---
+++ TrainGAN.py
@@ -0,0 +1,7 @@
+# import necessary packages
+
+
+# train the GAN
+def train(epochs, batch_size):
+    # function should train each of the ml files for a given number of epochs with a specified batch size of executables
+    pass
