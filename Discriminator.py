37aab6b54f7315dc51e3df7da77676afdd6ecba6
Will Macdonald <16wjgm@caslab.queensu.ca>
2019-10-29 12:23:45 -0400

    Including description of discriminator functionality

--- Discriminator.py
+++ Discriminator.py
@@ -3,4 +3,7 @@
 
 # initialize the discriminator of the GAN
 def init_discriminator():
+    # takes in inputs of either benign examples or malicious examples passed on from the generator
+    # discriminator uses different forms of classification to predict which type of example this is
+    # examples are passed from the discriminator to the detector to determine whether or not the executable is malicious
     pass
