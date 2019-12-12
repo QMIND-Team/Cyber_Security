2aeafd2a403c28f512bfe9cd38946b6da01bc78a
Will Macdonald <16wjgm@caslab.queensu.ca>
2019-12-11 19:53:28 -0500

    grouping metadata and vectorized features into groups (malicious, benign, unlabeled). Create histograms representing group distribution.

--- VisualizeData.py
+++ VisualizeData.py
@@ -1,46 +1,100 @@
 import os
 import ember
-import lightgbm as lgb
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+
+import warnings
+warnings.simplefilter('ignore')
+plt.rcParams.update(plt.rcParamsDefault)
 
 
-def init_dataset(dataset_dir):
+def init_vectorized_features(dataset_dir):
     ember.create_vectorized_features(dataset_dir, 1)
+
+
+def init_metadata(dataset_dir):
     ember.create_metadata(dataset_dir)
 
 
-def read_data(dataset_dir):
+def read_test_train(dataset_dir):
     X_train, y_train = ember.read_vectorized_features(dataset_dir, subset="train")
     print("Vectorized training features have been read")
     X_test, y_test = ember.read_vectorized_features(dataset_dir, subset="test")
+    print("Vectorized test features has been read")
+    return X_train, y_train, X_test, y_test
+
+
+def read_metadata(dataset_dir):
     metadata = ember.read_metadata(dataset_dir)
-    print("metadata has been read")
-    return X_train, y_train, X_test, y_test, metadata
+    print("Metadata has been read")
+    return metadata
 
 
-def train_ember_model(dataset_dir):
-    ember.create_vectorized_features(dataset, 1)
-    model = ember.train_model(dataset_dir)
-    return model
+def visualize_metadata(metadata_df):
+    print("Metadata")
+    meta_df = metadata_df.sample(frac=0.05, random_state=1)
+    malicious = meta_df[meta_df['label'] == 1]
+    benign = meta_df[meta_df['label'] == 0]
+    unlabeled = meta_df[meta_df['label'] == -1]
+    print("Number of Malicious Files: {}".format(len(malicious)))
+    print("Number of Benign Files: {}".format(len(benign)))
+    print("Number of Unlabeled Files: {}\n".format(len(unlabeled)))
+
+    mal_split = len(malicious) / len(meta_df)
+    ben_split = len(benign) / len(meta_df)
+    unlab_split = len(unlabeled) / len(meta_df)
+    print("Malicious File Ratio: {}".format(mal_split))
+    print("Benign File Ratio: {}".format(ben_split))
+    print("Unlabeled File Ratio: {}\n".format(unlab_split))
+
+    meta_df.hist(figsize=(50, 50), xlabelsize=80, ylabelsize=80)
+    plt.show()
+
+
+def visualize_vectorized_features(X_data, y_data):
+    print("Vectorized Features")
+    feat_list = []
+    for features in range(len(y_data)):
+        feat_list.append((X_data[features], y_data[features]))
+    feat_df = pd.DataFrame(feat_list)
+    feat_df.columns = ["Vectorized Features", "label"]
+
+    malicious = feat_df[feat_df['label'] == 1]
+    benign = feat_df[feat_df['label'] == 0]
+    unlabeled = feat_df[feat_df['label'] == -1]
+    print("Number of Malicious Files: {}".format(len(malicious)))
+    print("Number of Benign Files: {}".format(len(benign)))
+    print("Number of Unlabeled Files: {}\n".format(len(unlabeled)))
+
+    mal_split = len(malicious) / len(feat_df)
+    ben_split = len(benign) / len(feat_df)
+    unlab_split = len(unlabeled) / len(feat_df)
+    print("Malicious File Ratio: {}".format(mal_split))
+    print("Benign File Ratio: {}".format(ben_split))
+    print("Unlabeled File Ratio: {}".format(unlab_split))
+
+    feat_df.hist(figsize=(50, 50), xlabelsize=80, ylabelsize=80)
+    plt.show()
 
 
 if __name__ == '__main__':
     # todo change this file path for where you have ember dataset stored
     dataset = "D:/QMIND/DataSet/ember"
-    test_train_files = ['X_train.dat', 'y_train.dat', 'X_test.dat', 'y_test.dat', 'metadata.csv']
+    test_train_files = 'X_train.dat' and 'y_train.dat' and 'X_test.dat' and 'y_test.dat'
+    metadata_file = 'metadata.csv'
 
     dataset_dir_files = os.listdir(dataset)
     if test_train_files not in dataset_dir_files:
-        init_dataset(dataset)
-    X_train, y_train, X_test, y_test, metadata = read_data(dataset)
-    print("X_train data: {}".format(X_train))
-    print("y_train data: {}".format(y_train))
-    print("X_test data: {}".format(X_test))
-    print("y_test data: {}".format(y_test))
-
-
-    """
-    lgbm_model = train_ember_model(dataset)
-    lgbm_model = lgb.Booster(model_file=os.path.join(dataset, "ember_model_2017.txt"))
-    putty_data = open("~/putty.exe", "rb").read()
-    print(ember.predict_sample(lgbm_model, putty_data))
-    """
+        init_vectorized_features(dataset)
+    if metadata_file not in dataset_dir_files:
+        init_metadata(dataset)
+    X_train, y_train, X_test, y_test = read_test_train(dataset)
+    print("X_train data: \n{}\n".format(X_train))
+    print("y_train data: \n{}\n".format(y_train))
+    print("X_test data: \n{}\n".format(X_test))
+    print("y_test data: \n{}\n".format(y_test))
+    metadata = read_metadata(dataset)
+    print("Metadata Dataframe: \n{}\n".format(metadata))
+    visualize_metadata(metadata)
+    visualize_vectorized_features(X_train, y_train)
