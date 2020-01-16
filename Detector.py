48413927c1bca6df93a4ea3f1074ae18ff95bdbc
will1032 <willcoffell@gmail.com>
2020-01-16 13:45:12 -0500

    Made optimizer and chose a loss function

--- Detector.py
+++ Detector.py
@@ -1,19 +1,20 @@
-# import required packages
-from keras import optimizers
-from keras import models
-from keras import losses
+import tensorflow as tf
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 
-# initialize the detector model of the GAN
-def init_detector():
-    # take in output from the discriminator
-    # create fully connected neural network
-    # return model created
+def discriminator_loss(benign_examples, malicious_examples):
+    benign_loss = cross_entropy(tf.ones_like(benign_examples), benign_examples)
+    malicious_loss = cross_entropy(tf.zeros_like(malicious_examples), malicious_examples)
+    disc_loss = benign_loss + malicious_loss
+    return disc_loss
 
-    network = models.Sequential()
-    adam = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True)
-    logmse = losses.mean_squared_logarithmic_error(y_true, y_pred)
-    network.compile(optimizer=adam,
-                    loss=logmse,
-                    metrics=['accuracy'])
+
+def generator_loss(malicious_examples):
+    return cross_entropy(tf.ones_like(malicious_examples), malicious_examples)
+
+
+generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True)
+discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True)
+
 
