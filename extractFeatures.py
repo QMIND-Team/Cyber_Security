a49223e559b61ce94614d5329c3c03c69f3102e6
YarnSaw <44614514+YarnSaw@users.noreply.github.com>
2020-01-23 13:37:17 -0500

    updated with readMemmap function

--- extractFeatures.py
+++ extractFeatures.py
@@ -24,28 +24,17 @@ def vectorizePEs(inputPath):
     print("Finished creating the memmap for the data")
     data[:] = vectorizedFeatures[:]
     print("Finished mapping data to the memmap")
-    print(data[1])
-    print(data[1][1])
-    print(len(data[1]))
 
-
-
+def readMemmap(file):
+    #extracting data from a .dat file
+    extractor = ember.PEFeatureExtractor()
+    shape = (int(outPutFile.split("_")[0]),extractor.dim)
+    data = np.memmap(outPutFile, dtype=np.float32, mode="r+", shape=shape)
+    return data
 
 if __name__ == "__main__":
     dataPath = "C:\Programming\Github_projects\Ember\extraData"
     outPutFile = "13476_files_features.dat" #only needed to use when extracting features from memmap
     #file name is created in part by num of files that went into it, and is needed to later
     #parse the file into the proper sizing from reading the memmap
-
-    #creating the .dat file
-    #'''#comment and uncomment this docstring to switch from generating the memmap to testing it works
     vectorizePEs(dataPath)
-    '''
-    #extracting the data from the .dat file. 
-    extractor = ember.PEFeatureExtractor()
-    shape = (int(outPutFile.split("_")[0]),extractor.dim)
-    data = np.memmap(outPutFile, dtype=np.float32, mode="r+", shape=shape)
-    print(data[1])
-    print(data[1][1])
-    print(len(data[1]))
-    #'''
