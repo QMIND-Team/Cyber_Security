31c45f4454cab95a8536bbd12c7fcfc601f2ed43
Will Macdonald <willmacd@gmail.com>
2020-03-02 13:18:51 -0500

    visualize the malicious and benign predictions for the conference showcase

--- GAN.py
+++ GAN.py
@@ -1,4 +1,4 @@
-from Training.TrainGAN import train
+from Training.TrainGAN import train, display_training_predictions, plot_loss_functions
 
 EPOCHS = 20
 BATCH_SIZE_FLOOR = 391
@@ -6,42 +6,9 @@ BATCH_SIZE_FLOOR = 391
 
 # compile all the functionality of the GAN
 def GAN():
-    """
-    # TODO: Change this path to where Ember dataset is saved on respective computer
-    # dataset = "E:/QMIND/DataSet/ember"
-    # df = load_dataset(dataset)
-
-    noise = tf.random.uniform([1, 2381])
-    print("Noise Type: {}".format(type(noise)))
-    print("Noise Vector: {}".format(noise))
-    print("Noise Shape: {}\n".format(noise.shape))
-
-    # single_example = single_malicious_example(malicious_examples(df))
-    # example = tf.reshape(single_example, [1, 2381])
-    # print("Example Type: {}".format(type(noise)))
-    # print("Example Vector: {}".format(example))
-    # print("Example Shape: {}\n".format(example.shape))
-
-    # single_benign = single_benign_example(benign_examples(df))
-    # benign = tf.reshape(single_benign, [1, 2381])
-    # print("Benign Type: {}".format(type(benign)))
-    # print("Benign Vector: {}".format(benign))
-    # print("Benign Shape: {}\n".format(benign.shape))
-
-    example = tf.random.uniform([1, 2381])
-    print("Example Type: {}".format(type(example)))
-    print("Example Vector: {}".format(example))
-    print("Example Shape: {}\n".format(example.shape))
-
-    generator = init_generator()
-    adversarial_example = generate_example(example, noise, generator)
-    print(adversarial_example)
-
-    discriminator = init_discriminator()
-    predicted_label = discriminate_examples(adversarial_example, discriminator)
-    print(predicted_label)
-    """
-    train(EPOCHS, BATCH_SIZE_FLOOR, 5000)
+    generator, discriminator, predictions, loss_lists = train(EPOCHS, BATCH_SIZE_FLOOR, 5000)
+    display_training_predictions(predictions[0], predictions[1])
+    plot_loss_functions(loss_lists[0], loss_lists[1], EPOCHS)
 
 
 if __name__ == '__main__':
